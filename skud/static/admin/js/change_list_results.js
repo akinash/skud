77342e4f7ab76a0139cb283da9d6c779cfe39619
akinash <kinash@agima.ru>
2017-11-01 19:49:18 +0300

    Доработка среднего

--- skud/static/admin/js/change_list_results.js
+++ skud/static/admin/js/change_list_results.js
@@ -4,27 +4,27 @@
 (function($) {
 $(document).ready(function($) {
 
-    function avarage(field, field_text) {
+    function float_avarage(field, field_text) {
         body = $("body");
-        var hours_delay_average = 0;
-        var hours_delay_average_count = 0;
+        var average = 0;
+        var count = 0;
         field.each(function(){
             text = $(this).text();
             if(text !== '-' && text !== '0,0') {
-                hours_delay_average += parseFloat(text.replace(',', '.'));
-                hours_delay_average_count += 1;
+                average += parseFloat(text.replace(',', '.'));
+                count += 1;
             }
         });
-        hours_delay_average = (hours_delay_average/hours_delay_average_count).toFixed(2);
-        if(isNaN(hours_delay_average))
-            hours_delay_average = '0.0';
-        var replaced = body.html().replace('#' + field_text + '#', hours_delay_average);
+        average = (average/count).toFixed(2);
+        if(isNaN(average))
+            average = '0.0';
+        var replaced = body.html().replace('#' + field_text + '#', average);
         body.html(replaced);
     }
 
-    avarage($('.field-hours_delay'), 'field-hours_delay_average');
-    avarage($('.field-hours_way_out'), 'field-hours_way_out_average');
-    avarage($('.field-hours_duration'), 'field-hours_duration_average');
+    float_avarage($('.field-hours_delay'), 'field-hours_delay_average');
+    float_avarage($('.field-hours_way_out'), 'field-hours_way_out_average');
+    float_avarage($('.field-hours_duration'), 'field-hours_duration_average');
 
 });
 })(django.jQuery);
