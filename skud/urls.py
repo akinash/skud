655822c39052624ca84c754160dfad178395f76b
akinash <kinash@agima.ru>
2017-11-15 17:25:09 +0300

    templates

--- skud/urls.py
+++ skud/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from skud import views
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('^', include('django.contrib.auth.urls')),
+    #url('^', include('django.contrib.auth.urls')),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^login/$', auth_views.login, name='login'),
     url(r'^$', views.index, name='index'),
 ]
--- skud/views.py
+++ skud/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render_to_response, render
-import datetime
-from django.contrib.auth.decorators import login_required, permission_required
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
